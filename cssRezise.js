0f9fe2995dea8fea6f1b05fff171ba155b419684
Philip Olsson <friskis@Friskis-Air.lan>
2020-03-03 12:55:35 +0100

    tagit bort console loggar

--- cssRezise.js
+++ cssRezise.js
@@ -5,24 +5,21 @@ $(window).resize(function() {
     //let cssDesktop = document.querySelector(".cssDesktop")
     if (window.innerWidth > 600) {
       if(css.classList[1] == 'cssDesktop'){
-        console.log('Desktopversion redan innan')
+        //console.log('Desktopversion redan innan')
       }else {
-          let i
-          for(i=0; i < 1; i++) {
-              css.href = 'desktop.css'
-              css.classList.remove('cssMobile')
-              css.classList.add('cssDesktop')
-              console.log('Desktopversion nu' + this.innerWidth)
-          }
+        css.href = 'desktop.css'
+        css.classList.remove('cssMobile')
+        css.classList.add('cssDesktop')
+        console.log('Desktopversion nu' + this.innerWidth)
       }
     }else {
       if(css.classList[1] == 'cssMobile'){
-        console.log('Mobileversion redan innan')
+        //console.log('Mobileversion redan innan')
       }else {
         css.href = 'mobile.css'
         css.classList.remove('cssDesktop')
         css.classList.add('cssMobile')
-        console.log('Mobileversion nu')
+        console.log('Mobileversion nu'+ this.innerWidth)
       }
     }
     
--- cssToggle.js
+++ cssToggle.js
@@ -2,15 +2,12 @@ let css = document.querySelector(".css")
 let displayBody = document.querySelector("body")
 let i = 0
 if(i<1) {
-  console.log('1')
   if(window.innerWidth > 600){
-    console.log('2')
     css.href = 'desktop.css'
     css.classList.remove('cssMobile')
     css.classList.add('cssDesktop')
     console.log('dator')
   }else {
-    console.log('3')
     css.href = 'mobile.css'
     css.classList.remove('cssDesktop')
     css.classList.add('cssMobile')
@@ -18,5 +15,4 @@ if(i<1) {
   }
 }
 i++
-console.log('4')
 displayBody.classList.remove('displayNoneBody')
